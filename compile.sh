a56da08f880ff01dd2cbacf22fff0ae7e013d0cf
Artem Shalaev <artem.shalaev@gmail.com>
2011-12-22 14:28:07 +0400

    commit

---
+++ compile.sh
@@ -0,0 +1,1 @@
+java -jar tools/compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js sources/smarty.js --js_output_file sources/smarty-min.js
--- pack.sh
+++
@@ -1,1 +0,0 @@
-java -jar tools/yuicompressor-2.4.7.jar sources/smarty.js -o sources/smarty-min.js
