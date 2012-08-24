e84208440548423baec96fb6a5d923a9148fbe68
Stanislav Shlykov <stanislav.shlykov@gmail.com>
2012-08-24 18:04:00 +0400

    Fix: "for" operator iteration bug

--- sources/smarty.js
+++ sources/smarty.js
@@ -36,7 +36,7 @@
 	 * Function.bind()
 	 */
 	Function.prototype.bind || (Function.prototype.bind = function(oThis) {
-		if (typeof this !== "function") // closest thing possible to the ECMAScript 5 internal IsCallable function  
+		if (typeof this !== "function") // closest thing possible to the ECMAScript 5 internal IsCallable function
 		{
 			throw new TypeError("Function.prototype.bind - what is trying to be fBound is not callable");
 		}
@@ -61,46 +61,46 @@
 			throw new TypeError(" this is null or not defined");
 		}
 
-		// 1. Let O be the result of calling ToObject passing the |this| value as the argument.  
+		// 1. Let O be the result of calling ToObject passing the |this| value as the argument.
 		var O = Object(this);
 
-		// 2. Let lenValue be the result of calling the Get internal method of O with the argument "length".  
-		// 3. Let len be ToUint32(lenValue).  
+		// 2. Let lenValue be the result of calling the Get internal method of O with the argument "length".
+		// 3. Let len be ToUint32(lenValue).
 		var len = O.length >>> 0;
 
-		// 4. If IsCallable(callback) is false, throw a TypeError exception.  
-		// See: http://es5.github.com/#x9.11  
+		// 4. If IsCallable(callback) is false, throw a TypeError exception.
+		// See: http://es5.github.com/#x9.11
 		if ({}.toString.call(callback) != "[object Function]") {
 			throw new TypeError(callback + " is not a function");
 		}
 
-		// 5. If thisArg was supplied, let T be thisArg; else let T be undefined.  
+		// 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
 		if (thisArg) {
 			T = thisArg;
 		}
 
-		// 6. Let k be 0  
+		// 6. Let k be 0
 		k = 0;
 
-		// 7. Repeat, while k < len  
+		// 7. Repeat, while k < len
 		while (k < len) {
 			var kValue;
 
-			// a. Let Pk be ToString(k).  
-			//   This is implicit for LHS operands of the in operator  
-			// b. Let kPresent be the result of calling the HasProperty internal method of O with argument Pk.  
-			//   This step can be combined with c  
-			// c. If kPresent is true, then  
+			// a. Let Pk be ToString(k).
+			//   This is implicit for LHS operands of the in operator
+			// b. Let kPresent be the result of calling the HasProperty internal method of O with argument Pk.
+			//   This step can be combined with c
+			// c. If kPresent is true, then
 			if (k in O) {
 
-				// i. Let kValue be the result of calling the Get internal method of O with argument Pk.  
+				// i. Let kValue be the result of calling the Get internal method of O with argument Pk.
 				kValue = O[ k ];
 
-				// ii. Call the Call internal method of callback with T as the this value and  
-				// argument list containing kValue, k, and O.  
+				// ii. Call the Call internal method of callback with T as the this value and
+				// argument list containing kValue, k, and O.
 				callback.call(T, kValue, k, O);
 			}
-			// d. Increase k by 1.  
+			// d. Increase k by 1.
 			k++;
 		}
 		// 8. return undefined
@@ -2586,7 +2586,6 @@
 	/*
 	 * FOR operator
 	 */
-
 	smarty.addEntity('for', {
 		attributes: {
 			_required: ['from', 'to'],
@@ -2605,7 +2604,7 @@
 				result += smarty.utils.format("this.sv({0}, {1});", attr.name, meta);
 			}
 
-			result += smarty.utils.format("for(var {0} = {1}, {2} = {3}; {0} <= {2}; {0} += {4}){\
+			result += smarty.utils.format("for(var {0} = +{1}, {2} = +{3}; {0} <= {2}; {0} += +{4}){\
 			{5}.iteration++, smarty.utils.extend({5}, { index: {0} });", start, attr.from, end, attr.to, attr.step || 1, meta);
 
 			return result;
